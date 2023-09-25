eb789b99296d7aa6359b880955956862dfef122c
Jonathan DeMarks <jmdemarks@spscommerce.com>
2023-09-25 14:10:00 -0500

    prettify aarch64 sys_write

--- src/minilib/linux_aarch64.rs
+++ src/minilib/linux_aarch64.rs
@@ -7,8 +7,7 @@ unsafe extern "C" fn _start() {
 }
 
 pub unsafe fn sys_exit(exit_code: usize) -> ! {
-    asm!(
-        "svc 0",
+    asm!("svc 0",
         in("w8") 93,
         in("x0") exit_code,
         options(nostack, noreturn)
@@ -16,16 +15,11 @@ pub unsafe fn sys_exit(exit_code: usize) -> ! {
 }
 
 pub unsafe fn sys_write(buffer: *const u8, count: usize) {
-    asm!(
-        "mov  x0, #1",
-        "mov  x1, {buffer}",
-        "mov  x2, {count}",
-        "mov  x8, #64",
-        "svc #0",
-        buffer = inout(reg) buffer => _,
-        count = inout(reg) count => _,
-        out("x0") _,
-        out("x8") _,
+    asm!("svc #0",
+        inout("x0") 1 => _,
+        inout("x1") buffer => _,
+        inout("x2") count => _,
+        inout("x8") 64 => _,
         options(nostack)
     )
 }
